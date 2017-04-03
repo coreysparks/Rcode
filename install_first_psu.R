1a76b84f665ad99d17fb2104abc368c74608b72a
Corey Sparks <corey.sparks@utsa.edu>
2017-04-03 16:28:35 -0500

    Package install script
    for APDEM 802

---
+++ install_first_psu.R
@@ -0,0 +1,6 @@
+install.packages(c("lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv", "tigris", "RQGIS", "acs"), dependencies = T)
+library(ctv)
+install.views(c("SocialSciences", "Spatial"), coreOnly = T )
+#install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
+
+
