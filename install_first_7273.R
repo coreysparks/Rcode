9d50a0314c15beb07a6a4106d14567e082e18ab1
Corey Sparks <corey.sparks@utsa.edu>
2017-07-23 14:47:12 -0500

    dem7273_install packages

---
+++ install_first_7273.R
@@ -0,0 +1,6 @@
+install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot"), dependencies = T)
+library(ctv)
+install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
+#install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
+
+
