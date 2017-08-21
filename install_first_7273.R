3f70eb95a43657ca753565ee9a71107bc02f4bd9
Corey Sparks <corey.sparks@utsa.edu>
2017-08-21 08:58:34 -0500

    dem7273_first install

--- install_first_7273.R
+++ install_first_7273.R
@@ -1,4 +1,5 @@
-install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot"), dependencies = T)
+message("This may take a while")
+install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "survey", "devtools"), dependencies = T)
 library(ctv)
 install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
 #install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
