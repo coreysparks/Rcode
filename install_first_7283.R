3ee5b4460245325cc25864b939ba0aeea502557c
Corey Sparks <corey.sparks@utsa.edu>
2018-01-08 09:55:00 -0600

    install first for dem 7283
    installs libraries we will need in the course

---
+++ install_first_7283.R
@@ -0,0 +1,7 @@
+print("This may take a while")
+install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "survey", "devtools", "lme4", "survey", "mice", "pander", "knitr"), dependencies = T)
+library(ctv)
+install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
+#install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
+
+
