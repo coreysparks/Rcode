bd238d84476d74954b01912982eed589330f38d9
Corey Sparks <corey.sparks@utsa.edu>
2018-05-13 22:40:31 -0500

    updated
    added mapview

--- install_before_tamu_18.R
+++ install_before_tamu_18.R
@@ -3,7 +3,7 @@
 #It may take a minute to finish, that's normal
 print("This may take a while")
 
-install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "tidyverse","sf"), dep=T)
+install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "tidyverse","sf", "mapview"), dep=T)
 library(ctv)
 install.views(c("SocialSciences", "Spatial"), coreOnly = T)
 install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
