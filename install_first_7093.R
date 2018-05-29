682f94a4444876d21f7df81562114d4da38c48b3
Corey Sparks <corey.sparks@utsa.edu>
2018-05-29 11:27:29 -0500

    DEM 7093 su 18
    R packages for class

---
+++ install_first_7093.R
@@ -0,0 +1,6 @@
+print("DEM 7093 R packages - Install may take a while")
+install.packages(c("devtools", "tidyverse", "car", "Hmisc", "knitr", "lattice",  "survey", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "acs", "tigris", "spdep", "rsaga"), dependencies = T)
+library(ctv)
+install.views(c("SocialSciences", "ReproducibleResearch", "Spatial"), coreOnly = T )
+devtools::install_github("jannes-m/RQGIS", dep=TRUE)
+
