536c5e01bbf75b3420e5092b7502f875ec11bac4
coreysparks <corey.sparks@utsa.edu>
2020-08-18 14:45:11 -0500

    updated

--- install_first_7223.R
+++ install_first_7223.R
@@ -1,5 +1,5 @@
 print("This may take a while")
-install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "survey", "devtools", "muhaz", "coxme","eha", "cmprsk", "knitr"), dependencies = T)
+install.packages(c("devtools", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "survey", "devtools", "muhaz", "coxme","eha", "cmprsk", "knitr"), dependencies = T)
 library(ctv)
 install.views(c("SocialSciences", "ReproducibleResearch", "Survival"), coreOnly = T )
 install.packages("INLA", repos="https://inla.r-inla-download.org/R/stable")
