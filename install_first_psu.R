922b22164f94ba4d057575a1f51c457ede8570f8
Corey Sparks <corey.sparks@utsa.edu>
2020-12-17 09:09:28 -0600

    update

--- install_first_psu.R
+++ install_first_psu.R
@@ -1,4 +1,4 @@
-install.packages(c("lmtest", "car", "Hmisc", "sandwich", "multcomp","haven", "tidycensus", "tigris", "spatialreg", "knitr", "lattice", "lme4", "survey", "pscl", "ctv", "tigris", "RQGIS", "acs", "ggmap", "dplyr", "sjPlot", "sjstats", "lmerTest"), dependencies = T)
+install.packages(c("lmtest", "car", "Hmisc", "sandwich", "multcomp","haven","tidyverse", "tidycensus", "tigris", "spatialreg", "knitr", "lattice", "lme4", "survey", "pscl", "ctv", "tigris", "RQGIS", "acs", "ggmap", "dplyr", "sjPlot", "sjstats", "lmerTest"), dependencies = T)
 library(ctv)
 install.views(c("SocialSciences", "Spatial"), coreOnly = T )
 #install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
