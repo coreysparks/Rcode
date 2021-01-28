0377ea6e9bb61ce1e3c100cd0c7a0d7879b61a68
Corey Sparks <corey.sparks@utsa.edu>
2021-01-27 19:54:21 -0600

    Update install_first_7093.R

--- install_first_7093.R
+++ install_first_7093.R
@@ -1,5 +1,5 @@
 print("DEM 7093 R packages - Install may take a while")
-install.packages(c("devtools", "tidyverse", "car", "Hmisc", "knitr", "lattice","spatialreg",  "survey", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "acs", "tigris", "spdep", "RSAGA", "sf", "tidycensus"), dependencies = T)
+install.packages(c("devtools","tmap", "mapview", "tidyverse", "car", "Hmisc", "knitr", "lattice","spatialreg",  "survey", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "acs", "tigris", "spdep", "RSAGA", "sf", "tidycensus"), dependencies = T)
 
 ctv::install.views(c("SocialSciences", "ReproducibleResearch", "Spatial"), coreOnly = T )
 #devtools::install_github("jannes-m/RQGIS", dep=TRUE)
