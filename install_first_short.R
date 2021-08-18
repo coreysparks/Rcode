262019693d6f7092c82e20418463cebf8da102f7
Corey Sparks <corey.sparks@utsa.edu>
2021-08-18 10:54:48 -0500

    Update install_first_short.R

--- install_first_short.R
+++ install_first_short.R
@@ -1,8 +1,8 @@
 print("This may take a while, be patient. Also answer yes to any questions :)")
 Sys.sleep(3)
 
-install.packages(c("ctv", "devtools","tidycensus","censusapi","ipumsr", "sf", "tidyverse","bootstrap", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice","survey", "pscl", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "survey"), dependencies = T)
-library(ctv)
-install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
+install.packages(c("ctv", "devtools","tidyverse", "tidycensus", "knitr"), dependencies = T)
+#library(ctv)
+#install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
 
 
