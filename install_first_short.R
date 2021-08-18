b3f60fed104fdc2d739cf51bdf99d91bdabc38f0
Corey Sparks <corey.sparks@utsa.edu>
2021-08-18 18:09:08 -0500

    Update install_first_short.R

--- install_first_short.R
+++ install_first_short.R
@@ -1,7 +1,7 @@
 print("This may take a while, be patient. Also answer yes to any questions :)")
 Sys.sleep(3)
 
-install.packages(c("ctv", "devtools","tidyverse", "tidycensus", "knitr"), dependencies = T)
+install.packages(c("ctv", "devtools","tidyverse", "tidycensus","ggplot2", "knitr"), dependencies = T)
 #library(ctv)
 #install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
 
