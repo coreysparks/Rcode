147f4e755c655923ca9fb74b292af53587ea2f5d
Corey Sparks <corey.sparks@utsa.edu>
2022-01-06 09:42:16 -0600

    Update install_first_7283.R

--- install_first_7283.R
+++ install_first_7283.R
@@ -1,7 +1,8 @@
 print("This may take a while")
-install.packages(c("devtools", "tidyverse","bootstrap","tableone", "srvyr", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4", "survey", "pscl", "readstata13", "ctv","ggplot2", "acs", "ggmap", "dplyr", "sjPlot", "survey", "devtools", "lme4", "survey", "mice", "pander", "knitr", "questionr", "stargazer"), dependencies = T)
+
+install.packages(c("devtools", "tidyverse","bootstrap","tableone", "srvyr", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4","lmerTest", "survey", "ctv", "tidycensus", "ggmap", "sjPlot", "survey", "devtools", "lme4", "mice", "pander", "knitr", "questionr", "stargazer", "MetBrewer", "geepack", "patchwork", "haven"),
+                 dependencies = T)
 library(ctv)
 install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
-#install.packages("INLA", repos="https://www.math.ntnu.no/inla/R/stable")
 
 
