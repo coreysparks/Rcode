18f11e86ee967e4e709958b84d1394e52d8f20fb
Corey Sparks <corey.sparks@utsa.edu>
2022-01-24 12:49:23 -0600

    Update install_first_7283.R

--- install_first_7283.R
+++ install_first_7283.R
@@ -1,8 +1,9 @@
 print("This may take a while")
 
-install.packages(c("devtools", "tidyverse","bootstrap","tableone", "srvyr", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4","lmerTest", "survey", "ctv", "tidycensus", "ggmap", "sjPlot", "survey", "devtools", "lme4", "mice", "pander", "knitr", "questionr", "stargazer", "MetBrewer", "geepack", "patchwork", "haven", "gtsummary"),
+install.packages(c("devtools", "tidyverse","bootstrap","tableone", "lmtest", "car", "Hmisc", "sandwich", "multcomp", "knitr", "lattice", "lme4","lmerTest", "survey", "ctv", "tidycensus", "ggmap", "sjPlot", "survey", "devtools", "lme4", "mice", "pander", "knitr", "questionr", "stargazer", "MetBrewer", "geepack", "patchwork", "haven","srvyr", "gtsummary"),
                  dependencies = T)
 library(ctv)
-install.views(c("SocialSciences", "ReproducibleResearch"), coreOnly = T )
+install.views(c("SocialSciences", "ReproducibleResearch"),
+              coreOnly = T )
 
 
